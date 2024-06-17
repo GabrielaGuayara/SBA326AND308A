9bee78d2edd6c60f84c6d84d9f9f2791f8cff59f
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-16 20:04:37 -0400

    Retrieved data from an API

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const factList = document.querySelector(".facts-list")
 
 console.log(factList)
+
+const url = "https://api.thecatapi.com/v1/facts";
+const apiKey = "live_3ptZ6oXj6Gz7au1pT24sFNfqorps6olUzNrQnPOp9Gm1xxIEAFN8ZSdBhMph5EFc"
+
+fetch(url,{
+    headers:{
+        'x-api-key': apiKey
+    }
+}).then(resp => {
+    if(!resp.ok){
+        throw new Error('Error loading API')
+    }
+    return reportError.json();
+}).then(data =>{
+    console.log(data)
+})
