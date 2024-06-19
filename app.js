039013c7f0bcd2ac779ac607c08008a6b8c43d16
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-19 12:33:28 -0400

    Update variables' names and created new html elements in js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ defaulOption.setAttribute("value", "default")
 defaulOption.textContent = "Select a breed"
 selector.append(defaulOption)
 
-let newItem ="";
+let newCat ="";
 
 
 // const url = `https://api.thecatapi.com/v1/images/search?limit=10`;
 
-const url = `https://api.thecatapi.com/v1/breeds?limit=9`;
+const url = `https://api.thecatapi.com/v1/breeds?limit=5`;
 const api_key = "live_3ptZ6oXj6Gz7au1pT24sFNfqorps6olUzNrQnPOp9Gm1xxIEAFN8ZSdBhMph5EFc"
     
 async function getDateByBreed(){
@@ -38,45 +38,51 @@ try{
             selector.append(option)
         })
 
+        const seeAllCats = document.createElement("option")
+        seeAllCats.setAttribute("value", "seeAll")
+        seeAllCats.innerText = "See All Cats"
+        selector.append(seeAllCats)
+
         selector.addEventListener('change', function() {
            
             const selectedBreedId = this.value; // `this` refers to the select element
-            const findBreed = data.find(breed => breed.id === selectedBreedId);
     
-            if (findBreed) {
-                
-                data.forEach(breed => {
-                    newItem = "";
-                    newItem = `
-                    <div class="card m-2" style="width: 18rem;">
-                        <img class="card-img-top" src="${breed.image.url}" style="height: 40%; weight: 100%" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">${breed.name}</h5>
-                            <p class="card-text">${breed.description}</p>
-                            <p class="card-text" style="font-weight: 500">${breed.temperament}</p>
-                        </div>
-                    </div>`;
-                });
-    
-                // Assuming you have an element with id 'breedContainer' where you want to display the cards
-                gallery.innerHTML = newItem;
-
+            if(selectedBreedId === 'seeAll'){
+                newCat = "";
+                factory(data)
+            }else{
+                const findBreed = data.find(breed => breed.id === selectedBreedId);
                 console.log(findBreed)
-                }})
-            } else {
-                console.log('Please select a valid breed');
-            }
+                    if (findBreed) {
+                        
+                        data.forEach(breed => {
+                            newCat = `
+                            <div class="card m-2" style="width: 18rem;">
+                                <img class="card-img-top" src="${findBreed.image.url}" style="height: 40%; weight: 100%" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">${findBreed.name}</h5>
+                                    <p class="card-text">${findBreed.description}</p>
+                                    <p class="card-text" style="font-weight: 500">${findBreed.temperament}</p>
+                                </div>
+                            </div>`;
+                        });
+            
+                        // Assuming you have an element with id 'breedContainer' where you want to display the cards
+                        gallery.innerHTML = newCat;
 
-        }catch(error){
+                        }}})
+}
+                }catch(error){
             console.log(error)
         }
 
 }
 
 const factory = (data) =>{
+
     data.forEach(element => {
        
-        newItem += `
+        newCat += `
         <div class="card m-2" style="width: 18rem;">
             <img class="card-img-top" src="${element.image.url}" style="height: 40%; weight: 100%"alt="Card image cap">
         <div class="card-body">
@@ -88,8 +94,10 @@ const factory = (data) =>{
         
         `;
     })
-        gallery.innerHTML = newItem;
+        gallery.innerHTML = newCat;
     }
 
-getDateByBreed();
+// getDateByBreed();
+
+
 
